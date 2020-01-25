22d87c9e30ceeec8c9ddce9e2e498a3d87c27fc2
Ernesto Arellano <ernestoarellano@icloud.com>
2020-01-24 16:55:43 -0800

    add package to enable blade support inside acf blocks

---
+++ resources/views/blocks/hero.blade.php
@@ -0,0 +1,11 @@
+{{--
+  Title: Hero
+  Description: A hero block.
+  Category: common
+  Icon: admin-comments
+  Keywords: hero
+  Mode: edit
+  Align: full
+--}}
+
+{!! get_field('hero_content') !!}
